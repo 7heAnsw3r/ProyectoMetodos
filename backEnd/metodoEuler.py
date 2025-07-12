1dacd1550a2caa173248052a934eab8ea3564baf
Nattyrd <kevineduardo2508@gmail.com>
2025-07-11 23:33:49 -0500

    Arreglo de Instancia y funciones
    Arregle la instancia y funciones del metodo. Debido a que al unirlo a trabajar directamente en la main. se tuvo que cambiar algunos detalles para poder evitar problemas al momento de usar el metodo junto con Minimos Cuadrados.

--- backEnd/metodoEuler.py
+++ backEnd/metodoEuler.py
@@ -6,21 +6,28 @@ class MetodoEuler:
         self.open_file.open()
         self.data = self.open_file.data()
 
-def derivar(self, coef): #Obtenemos la derivada con los coef de nuestro polinomio.
-    derivada =[j * coef[j] for j in range(1,len(coef))]
-    return derivada
 
-def evaluarP(self, coe, x): # Le vamos a evaluar el polinomio segun un punto X especifico.
-    return sum(coe[j]*(x ** j) for j in range(len(coe)))
+    """def solution(self):
+    x = self.data[0]  # Asumiendo que la primera columna es x es decir High
+    y = self.data[1]  # Asumiendo que la segunda columna es y es decir Low
+    return"""
 
-def eulermetodo(self, coe, x0, h): #Aplicamos el metodo euler al polinomio obtenido.
-    derivada = self.derivar(coe) #Obtenemos la derivada
+    def derivar(self, coef): #Obtenemos la derivada con los coef de nuestro polinomio.
+       derivada =[j * coef[j] for j in range(1,len(coef))]
+       return derivada
 
-    y0= self.evaluarP(coe, x0) #Obtenemos el polinomio evaluado en x0
+    def evaluarP(self, coe, x): # Le vamos a evaluar el polinomio segun un punto X especifico.
+        return sum(coe[j]*(x ** j) for j in range(len(coe)))
 
-    dydx = self.evaluarP(derivada, x0) # Evaluamos respecto al valor de la derivada
-    y1= y0 +  h * dydx # Aplicamos el metodo de Euler
 
-    return y1
+    def euler_met(self, coe, x0, h):
+        derivada = self.derivar(coe)  # Obtenemos la derivada
 
-# Esta es la logica basica que aplicamos  directamente a euler
+        y0 = self.evaluarP(coe, x0)  # Obtenemos el polinomio evaluado en x0
+
+        dydx = self.evaluarP(derivada, x0)  # Evaluamos respecto al valor de la derivada
+        y1 = y0 + h * dydx  # Aplicamos el metodo de Euler
+
+        return y1
+
+     # Esta es la logica basica que aplicamos  directamente a euler
